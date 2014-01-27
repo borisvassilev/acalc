06b4ff4a086b03f8ad3a6a36cd7cb13cb10ea333
Boris Vassilev <boris.vassilev@gmail.com>
2014-01-27 14:27:45 +0200

    Correct reading of a line of numbers (integers), treat trailing chars

--- acal.c
+++ acal.c
@@ -3,40 +3,120 @@
 #include <gmp.h>
 
 int read_number(mpz_t);
-void eat_blanks();
+int eat_space_to_nl(FILE *);
+int read_line(FILE *);
+int echo_line(FILE *);
+int read_num_line(FILE *);
+void trailing_error(FILE *, int);
 
 int main(void)
 {
-    mpz_t prod, cur;
-    mpz_inits(prod, cur);
-
-    mpz_set_ui(prod, 1);
-    int r;
-    while((r = read_number(cur)) && r != EOF)
-        mpz_mul(prod, prod, cur);
-
-    gmp_printf("%Zd\n", prod);
+    while (read_line(stdin) >= 0)
+        ;
     return 0;
 }
 
-void eat_blanks() /* is it really necessary? */
+/* Read a line from input, return number of tokens read
+ * or a negative value if an error ocurred.
+ */
+int read_line(FILE *stream)
 {
+    int c = eat_space_to_nl(stream);
+    int r;
+
+    if (c == EOF)
+        r = -1;
+    else if (isdigit(c)) { /* num line */
+        ungetc(c, stream);
+        r = read_num_line(stream);
+    } else if (c == '\n') { /* empty line */
+        putc('\n', stdout);
+        r = 0;
+    } else {
+        putc(c, stdout);
+        r = echo_line(stream);
+    }
+    fflush(NULL);
+    return r;
+}
+
+/* Echo up to end of line from input stream to stdout
+ */
+int echo_line(FILE *stream)
+{
+    int c, i;
+    for (i = 0; (c = getc(stream)) != '\n' && c != EOF; ++i)
+        putc(c, stdout);
+    putc('\n', stdout);
+    return i;
+}
+
+/* Read numbers separated by blanks up to end of line
+ * or end of file.
+ */
+int read_num_line(FILE *stream)
+{
+    mpz_t num;
+    mpz_init(num);
+
+    /* there is at least one digit in stream */
+    mpz_inp_str(num, stream, 10);
+    gmp_printf("%Zd", num);
+
     int c;
-    while((c = getc(stdin)) != EOF)
-        if (!isblank(c))
-        {   ungetc(c, stdin);
+    int i = 0;
+    while ((c = eat_space_to_nl(stream)) != '\n' && c != EOF)
+        if (isdigit(c)) {
+            ungetc(c, stream);
+            mpz_inp_str(num, stream, 10);
+            ++i;
+            gmp_printf(" %Zd", num);
+        } else {
+            trailing_error(stream, c);
             break;
         }
+
+    putc('\n', stdout);
+    mpz_clear(num);
+    return i;
 }
 
-int read_number(mpz_t num /* must be initialized */)
+/* Consume all trailing characters up to end of line or
+ * end of file. Send the beginning to stderr.
+ */
+void trailing_error(FILE *stream, int c)
 {
-    int c, r;
+    const int max = 27;
+    int i = 0;
 
-    if ((c = getc(stdin)) != EOF)
-    {   ungetc(c, stdin);
-        r = mpz_inp_str(num, NULL, 10);
-        return r;
+    fprintf(stderr, "trailing chars: ");
+    putc(c, stderr);
+    for (i = 0; i < max; ++i) {
+        c = getc(stream);
+        if (c == '\n' || c == EOF) {
+            putc('\n', stderr);
+            return;
+        }
+        putc(c, stderr);
     }
+    fprintf(stderr, "...\n");
+    while ((c = getc(stream)) != '\n' && c != EOF)
+        ;
+}
+
+/* Consume white characters up to end of line and return the
+ * first non-blank character
+ */
+int eat_space_to_nl(FILE *stream)
+{
+    int c;
+    while ((c = getc(stream)) != EOF)
+        if (!isspace(c) || c == '\n')
+            return c;
+        /* for debugging */
+        else
+            putc('.', stdout);
+        /* */
     return EOF;
 }
+
